548fd08ca1e2eaf16b7d30077e5fd7ffe145f86f
Milan Kumawat <milankumawat01@gmail.com>
2025-05-11 17:40:12 +0530

    done

--- home/models.py
+++ home/models.py
@@ -13,12 +13,13 @@ class Person(models.Model):
     zip = models.CharField(max_length=100)
 
 
-class Product(models.Model):
-   
-    pass    # name = models.CharField(max_length=100)
-    # price = models.DecimalField(max_digits=10, decimal_places=2)
-    # description = models.TextField()
-    # image = models.ImageField()
-    # created_at = models.DateTimeField(auto_now_add=True)
+class Car(models.Model):
+    car_name = models.CharField(max_length=100)
+    speed = models.IntegerField(default=50)
+
+    def __str__(self):
+        return self.car_name
+
+    
     
     
---
+++ home/utils.py
@@ -0,0 +1,8 @@
+from home.models import Person
+
+import time
+
+def run_this_function():
+    print("Running this function")
+    time.sleep(2)
+    print("Done")
